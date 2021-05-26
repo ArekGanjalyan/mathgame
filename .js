b079d27ce0620877ca3e06c6996e0f8f56613a4b
ArekGanjalyan <a.ganjalyan@gmail.com>
2021-05-26 23:51:16 +0200

    Added functionality

--- .js
+++ .js
@@ -1,12 +1,28 @@
-var answer = 0;
-
-function createProblem() {
-  var num1: Math.floor(Math.random() * 11);
-  var num2: Math.floor(Math.random() * 11);
-  answer = num1 + num2
+const form = document.getElementById("form")
+var num1 = 0;
+var num2 = 0;
+function generateProblem(){
+  num1 = Math.floor(Math.random() * 101);
+  num2 = Math.floor(Math.random() * 101);
   document.getElementById("num1").innerHTML = num1;
   document.getElementById("num2").innerHTML = num2;
 }
+generateProblem()
 
-createProblem()
- 
+form.addEventListener("submit", submit)
+function submit(event) {
+  event.preventDefault()
+  var correct_answer = num1 + num2
+  var form_answer = document.getElementById("answer").value
+  var user_answer = parseInt(form_answer, 10)
+  document.getElementById("evaluation").innerHTML = num1;
+  if (user_answer == correct_answer){
+    document.getElementById("answer").value = "";
+    document.getElementById("evaluation").innerHTML = "That's correct!";
+    generateProblem()
+  }
+  else{
+    document.getElementById("answer").value = "";
+    document.getElementById("evaluation").innerHTML = "That's incorrect! Try again.";
+  }
+}
